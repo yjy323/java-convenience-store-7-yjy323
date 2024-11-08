037fa5a44c560d0f17d175eb26c662cd83682e95
JooYoung <beachu23@naver.com>
2024-11-08 17:40:39 +0900

    feat(view): 파일 입력 기능을 추가하라
    주어진 경로의 파일 내용을 읽어온다.
    파일의 내용을 한 줄 씩 읽어 List<String>으로 저장한다.
    
    BREAKING CHANGE:
    	변경 사항:
    	- 파일을 읽어오는 FIleInputView 클래스를 구현한다.
    
    	예외 처리:
    	- 존재하지 않는 파일이라면 예외 메시지를 출력한다.
    	- 파일을 읽어오는 중 IOException이 발생한다면 예외 메시지를 출력한다.

---
+++ src/main/java/store/view/FileInputView.java
@@ -0,0 +1,27 @@
+package store.view;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileInputView {
+
+    public List<String> read(String fileName) {
+        List<String> lines = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            br.readLine();
+            String line = br.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = br.readLine();
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return lines;
+    }
+}
