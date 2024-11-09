cdb52e5ab73a88f13f1c19d4c552f9cc3d09afaa
JooYoung <beachu23@naver.com>
2024-11-09 14:17:29 +0900

    feat(FileLoader): 파일 불러오기 기능을 추가하라
    상품 및 프로모션 정보를 파일에서 읽어오기 위해 주어진 경로의 파일 내용을 불러오는 기능을 구현한다.
    
    BREAKING CHANGE:
    	변경 사항:
    	- FileLoader 클래스를 통해 파일 내용을 읽는다.
    	- 읽어온 내용은 List<String> 자료구조에 저장한다.
    
    	예외 처리:
    	- 잘못된 파일 경로라면 IllegalArgumentException이 발생한다.
    	- 파일을 읽는 중 예외가 발생하면 IllegalArgumentException로 처리한다.

--- src/main/java/store/view/FileLoader.java
+++ src/main/java/store/view/FileLoader.java
@@ -1,26 +1,41 @@
 package store.view;
 
+import static store.ErrorMessages.FILE_OPEN_FAIL;
+import static store.ErrorMessages.FILE_READ_FAIL;
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileInputView {
+public class FileLoader {
+
+    private FileReader openFile(String fileName) {
+        try {
+            return new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(String.format(FILE_OPEN_FAIL.getMessage(), fileName));
+        }
+    }
+
+    private List<String> readContents(BufferedReader br) throws IOException {
+        List<String> contents = new ArrayList<>();
+        String line = br.readLine();
+        while (line != null) {
+            contents.add(line);
+            line = br.readLine();
+        }
+        return contents;
+    }
 
     public List<String> read(String fileName) {
-        List<String> lines = new ArrayList<>();
+        BufferedReader br = new BufferedReader(openFile(fileName));
         try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-            String line = br.readLine();
-            while (line != null) {
-                lines.add(line);
-                line = br.readLine();
-            }
-            br.close();
+            return readContents(br);
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            throw new IllegalArgumentException(String.format(FILE_READ_FAIL.getMessage(), fileName));
         }
-        return lines;
     }
 }
