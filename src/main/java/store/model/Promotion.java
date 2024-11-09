340a2ca14fa902495c2fb67dad7f35af0d1a8993
JooYoung <beachu23@naver.com>
2024-11-09 14:26:31 +0900

    refactor(Promotion): 프로모션 불러오기 기능을 리팩토링하라
    클래스 구조 변화에 따라 Promotion을 리팩토링한다.
    
    BREAKING CHANGE:
    	변경 사항:
    	- 프로모션 고유의 정보를 저장한다.
    	- 프로모션 고유의 예외를 처리한다.
    
    	예외 처리:
    	- 시작일자와 종료일자 범위가 유효하지 않다면 IllegalArgumentException이 발생한다.

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -1,20 +1,23 @@
-package store.domain;
+package store.model;
 
-import java.time.LocalDateTime;
+import static store.ErrorMessages.INVALID_DATE_RANGE;
+
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Promotion {
 
     public static final int BUY_MIN_VALUE = 1;
     public static final int FREE_MIN_VALUE = 1;
-    
+    public static final String NON_PROMOTION = "null";
+
     private String name;
     private int buy;
     private int free;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    private LocalDate startTime;
+    private LocalDate endTime;
 
-    public Promotion(String name, int buy, int free, LocalDateTime startTime, LocalDateTime endTime) {
+    public Promotion(String name, int buy, int free, LocalDate startTime, LocalDate endTime) {
         validate(name, buy, free, startTime, endTime);
         this.name = name;
         this.buy = buy;
@@ -23,13 +26,13 @@ public class Promotion {
         this.endTime = endTime;
     }
 
-    private void validate(String name, int buy, int free, LocalDateTime startTime, LocalDateTime endTime) {
+    private void validate(String name, int buy, int free, LocalDate startTime, LocalDate endTime) {
         validateDateRange(startTime, endTime);
     }
 
-    private void validateDateRange(LocalDateTime startTime, LocalDateTime endTime) {
+    private void validateDateRange(LocalDate startTime, LocalDate endTime) {
         if (startTime.isAfter(endTime)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_DATE_RANGE.getMessage());
         }
     }
 
@@ -48,11 +51,11 @@ public class Promotion {
         return free;
     }
 
-    public LocalDateTime getStartTime() {
+    public LocalDate getStartTime() {
         return startTime;
     }
 
-    public LocalDateTime getEndTime() {
+    public LocalDate getEndTime() {
         return endTime;
     }
 
